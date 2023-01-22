4ad46746aeb9aaebb7f1f485f8e8650740bc07c5
tadcos29 <taddeo.costanzo@alumni.utoronto.ca>
2023-01-22 17:14:28 -0500

    Add game state control. Add high score display logic.

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,13 +11,18 @@ function init() {
     let answerFieldEl = document.getElementById('answer-field');
     let initFieldEl = document.getElementById('initials-field');
     let timerFieldEl = document.getElementById('timer-field');
+    let hsButtEl= document.getElementById('HS-button');
+
+    let bTimerActive=false;
+    let bGameState=false;
     questFieldEl.innerHTML="al";
 
     startButtEl.addEventListener("click", InitGame);
-
+    hsButtEl.addEventListener("click", ShowHS);
 
     function InitGame() {
-        let nTimerActive=true;
+        bGameState=true;
+        bTimerActive=true;
         let timeLeft=60;
         questFieldEl.innerHTML=objQuizBank.answers[1];
         let timerInterval=setInterval(AdvanceTime, 1000);
@@ -25,16 +30,26 @@ function init() {
 
 
         function AdvanceTime() {
-            if (nTimerActive) {
+            if (bTimerActive) {
                 timeLeft--;
             }
             timerFieldEl.innerHTML=timeLeft;
-            if (timeLeft<=0) {clearInterval(timerInterval);}
+            if (timeLeft<=0) {clearInterval(timerInterval);answerFieldEl.innerHTML="Time is up."; gameState=false;}
+            
         }
+        
 
     }
 
-   
+    function ShowHS() {
+        console.log("reached showhs")
+        if (bGameState){
+            if (bTimerActive) {bTimerActive=false;initFieldEl.innerHTML="Showing this, pausing timer."} 
+            else { bTimerActive=true;initFieldEl.innerHTML="Game back on."}
+        } else {initFieldEl.innerHTML="Showing this, game is not on."}
+        return
+
+    }
 
 }
 
